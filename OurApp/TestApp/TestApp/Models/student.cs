5517a4c2968c133b7625a085b417972c71b77702
ashwincmenon <ashwincmenon@gmail.com>
2017-12-09 02:11:14 -0800

    myfirstcommit

---
+++ OurApp/TestApp/TestApp/Models/student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestApp.Models
+{
+    public class student
+    {
+        public int rollno;
+        public string name;
+
+        public List<student> initStudent()
+        {
+            List<student> students = new List<student>();
+            students.Add(new student() { rollno = 10, name = "jithin" });
+            students.Add(new student() { rollno = 20, name = "ashwin" });
+            students.Add(new student() { rollno = 30, name = "davis" });
+
+            return students;
+        }
+    }
+}
